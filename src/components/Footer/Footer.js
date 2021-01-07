069a9a346390cb8e76199b176e366d93ac6b7731
KlerKpn <59790939+KlerKpn@users.noreply.github.com>
2021-01-07 15:24:07 +0300

    Footer UI

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,26 +4,65 @@ import Logo from '../../assets/image/Logo';
 import AppStore from '../../assets/image/iconAppStore.png'
 import GooglePlay from '../../assets/image/iconGooglePlay.png'
 
-const Footer =()=>{
+const Footer = () => {
     const cls = [
         'container',
         classes.Column
     ]
-    return(
+    return (
         <footer className={classes.Footer}>
             <div className={cls.join(' ')}>
                 <div className={classes.logo}>
                     <Logo />
                 </div>
                 <div className={classes.About}>
-                    ds
+                    <div className={classes.Footer_column}>
+                        <select defaultValue='Russian'>
+                            <option value='Russian'>Russian</option>
+                            <option value='English'>English</option>
+                        </select>
+                        <div>
+
+                        </div>
+                    </div>
+                    <div className={classes.Footer_column}>
+                        <ul>
+                            <li>
+                                About Eats
+                            </li>
+                            <li>
+                                Become a Delivery Partner
+                            </li>
+                            <li>
+                                Become a Restaurant Partner
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={classes.Footer_column}>
+                        <ul>
+                            <li>
+                                See all cities
+                            </li>
+                            <li>
+                                Pricing
+                            </li>
+                            <li>
+                                Help
+                            </li>
+                            <li>
+                               FAQs
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div className={classes.Store}>
-                    <img src={AppStore} alt='App Store'/>
-                    <img src={GooglePlay} alt='Google Play'/>
+                    <img src={AppStore} alt='App Store' />
+                    <img src={GooglePlay} alt='Google Play' />
                 </div>
                 <div className={classes.Privacy}>
-                    gdf
+                    <div className={classes.Privacy_block}>2021 Technologies Inc.</div>
+                    <div className={classes.Privacy_block}>Privacy</div>
+                    <div className={classes.Privacy_block}>Terms</div>
                 </div>
             </div>
         </footer>
